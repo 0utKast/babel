ed132982e62a78865b4a989997440bafa504e917
0utKast <educaleft@gmail.com>
2019-02-21 03:45:33 +0000

    Babel Final Sin flecha.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import http from "http"; // ES6 syntax
-double = a => a * 2
 const requestHandler = function(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello, World!");
